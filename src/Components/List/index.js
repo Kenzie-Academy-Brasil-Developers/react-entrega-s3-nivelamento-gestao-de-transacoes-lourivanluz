11002cb46ae234bed086e79e6d1b90deeac0e885
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-09-29 16:02:46 -0300

    testando

--- src/Components/List/index.js
+++ src/Components/List/index.js
@@ -6,8 +6,8 @@ export const List = ({ list, name }) => {
     <DivStyled>
       <h1>{name}</h1>
       <ul>
-        {list.map((item, index) => (
-          <li key={index}>
+        {list.map((item, indes) => (
+          <li key={indes}>
             <CardFrutas item={item} />
           </li>
         ))}
